8128328159c3d7835017b247f0b0cd71bdff8c3f
Arif Hossan <arif.hossan.eu@gmail.com>
2023-01-16 05:22:38 +0600

    double equal vs triple equal

---
+++ doubleEqualvstripleEqual.js
@@ -0,0 +1,17 @@
+/*
+double equal just check the value and triple equal check value and type
+1=true;0=false
+*/
+let a = 2;//number
+let b = "2";//string
+// if (a==b){
+//     console.log("true");
+// }
+// else {
+//     console.log("false");
+// }
+if (a === b) {
+  console.log("true");
+} else {
+  console.log("false");
+}
